bd554cd91d80e7d8c1bfdafbff4d2c61906727e7
CptMcSplody <1944639+CptMcSplody@users.noreply.github.com>
2018-08-25 16:19:35 +0200

    Fix Duplicate Records In Stats Parsing
    'criticalEffect': (bolt.Path(u'Skyrim.esm'), 0)
    
    Critical effect is a FormID but was being parsed as an int.
    Which meant the comparision for a change always gave a result of true.
    When led to a high number of ITM when using the stats patcher.
    
    EDITED:
     - Took out a minor unrelated formatting change.

--- Mopy/bash/parsers.py
+++ Mopy/bash/parsers.py
@@ -1395,8 +1395,7 @@ class ItemStats:
                               'stagger':self.sfloat,
                               'enchantPoints':self.sint,
                               'critDamage':self.sint,
-                              'criticalMultiplier':self.sfloat,
-                              'criticalEffect':self.sint,}
+                              'criticalMultiplier':self.sfloat,}
         elif bush.game.fsName in (u'FalloutNV', u'Fallout3'):
             self.attr_type = {'eid':self.sstr,
                               'weight':self.sfloat,
